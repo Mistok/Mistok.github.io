bb52e33fbaab4f655f9fb9e68edc009f6b5c0110
mistok <Mistreated@ukr.net>
2021-06-15 16:45:34 +0300

    added feedback page

--- WorkNest/app/js/main.js
+++ WorkNest/app/js/main.js
@@ -1,11 +1,16 @@
 $(function () {
 	let equalHeight = () => {
-		let heightStart = document.getElementById('about_block_title').getBoundingClientRect().top;
-		let heightEnd = document.getElementById('about_block_privacy').getBoundingClientRect().bottom;
+		if(document.documentElement.clientWidth > 780){
+			let heightStart = document.getElementById('about_block_title').getBoundingClientRect().top;
+			let heightEnd = document.getElementById('about_block_privacy').getBoundingClientRect().bottom;
 
-		let newHeight = heightEnd - heightStart
-		let secondColl = document.getElementById('content_block_scrollable');
-		secondColl.style.maxHeight = newHeight + 'px' ;
+			let newHeight = heightEnd - heightStart
+			let secondColl = document.getElementById('content_block_scrollable');
+			secondColl.style.maxHeight = newHeight + 'px' ;
+		} else {
+			let secondColl = document.getElementById('content_block_scrollable');
+			secondColl.style.maxHeight = 200 + 'vh' ;
+		}
 	}
 	equalHeight()
 })
